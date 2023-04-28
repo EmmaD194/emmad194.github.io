c8263f957642eb36ef014a7f13e373c63cfa41fa
Emma Ruth Daly <60703785+EmmaD194@users.noreply.github.com>
2023-04-28 22:49:35 +0100

    updated review section

--- assets/js/research.js
+++ assets/js/research.js
@@ -4,24 +4,24 @@ const researchTable = document.querySelector(".main");
 
 const research = [
   {
-    title: "A Batch Normalized Inference Network Keeps the KL Vanishing Away",
+    title: "'Working with Emma was stress free and I would highly recommend anyone who wants a hassle free website to work with her'",
     authors:
-      "Qile Zhu, Wei Bi, Xiaojiang Liu, Xiyao Ma, Xiaolin Li and Dapeng Wu",
+      "I approached Emma about the possibility of her designing a website for my dance school and within 24 hours she had a sample ready. She applied everything that I wanted and made some fantastic suggestions so that my website reflects my school. Working with Emma was stress free and I would highly recommend anyone who wants a hassle free website to work with her. She is passionate about her clients and it shows in her work",
     conferences:
-      "The 58th Annual Meeting of the Association for Computational Linguistics",
-    researchYr: 2020,
-    citebox: "popup1",
-    image: "assets/images/research-page/inteferenceNetwork.png",
+      "- Rebecca (Hughes Academy)",
+    researchYr: 2023,
+    citebox: "",
+    image: "assets/images/hugheslogo.png",
     citation: {
       vancouver:
-        "Qile Zhu, Wei Bi, Xiaojiang Liu, Xiyao Ma, Xiaolin Li and Dapeng Wu. A Batch Normalized Inference Network Keeps the KL Vanishing Away. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
+        "",
     },
     abstract:
-      "This is currently left empty and this can be considered as a dummy data 1",
-    absbox: "absPopup1",
+      "",
+    absbox: "",
   },
 
-  {
+  /*{
     title: "A Call for More Rigor in Unsupervised Cross-lingual Learning",
     authors:
       "Mikel Artetxe, Sebastian Ruder, Dani Yogatama, Gorka Labaka and Eneko Agirre",
@@ -144,7 +144,7 @@ const research = [
     abstract:
       "This is currently left empty and this can be considered as a dummy data 8",
     absbox: "absPopup8",
-  },
+  },*/
 ];
 AOS.init();
 const fillData = () => {
@@ -171,32 +171,15 @@ const fillData = () => {
                         </span>
                     </div>
                     <a href="#0" class="paperTitle"> ${title} </a> 
+                    <br></br>
                     <div class = "authors"> ${authors} </div> 
                     
                     <div class="rConferences"> ${conferences} 
                         <div class="researchY">${researchYr}</div>
                     </div>
                     
-                    <!--CITE BUTTON-->
-                    <div class="d-flex" style="margin-right:5%;">
-                        <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${absbox}" aria-expanded="false" aria-controls="${absbox}">
-                            ABSTRACT
-                        </button>
-                
-                        <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${citebox}" aria-expanded="false" aria-controls="${citebox}">
-                            CITE
-                        </button>
-                    </div>
-                    <div id="${absbox}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse">
-                        <div class="card-body">
-                            ${abstract}    
-                        </div>
-                    </div>
-                    <div id="${citebox}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse">
-                        <div class="card-body">
-                            ${citation.vancouver}    
-                        </div>
-                    </div>
+                    
+                    
                 </td>
             </tr>`)
   );
